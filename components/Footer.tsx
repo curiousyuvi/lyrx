962bf510e5fe586568b6462c63dd9b580caebac8
Yuvraj Singh <ysgaur9919@gmail.com>
2022-01-30 03:13:38 +0530

    fix: added correct github repo url to contribute anchor

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,7 +7,7 @@ const Footer:FC = function () {
     return (<div className="h-32 w-full bg-bottom bg-cover bg-[url('../public/music.jpg')] flex flex-col justify-center relative">
         <div className="h-32 backdrop-blur w-full bg-black/20 absolute left top z-10"></div>
        <div className="z-20 flex flex-col items-center justify-center"> <h1 className="text-white text-xl font-semibold " >Made with <Link href="https://nextjs.org/"><a className="text-sky-500 decoration-4 hover:underline" target="_blank">Next.js</a></Link></h1>
-    <Link href="https://react-icons.github.io/react-icons/search?q=github" passHref><a className="text-white/70 hover:text-pink-500" target="_blank"><div className="mt-4 flex items-center justify-center "><FaGithub className="text-xl inline-block"/><h1 className="ml-2 font-semibold inline-block">Contribute</h1></div></a></Link></div>
+    <Link href="https://github.com/curiousyuvi/lyrx" passHref><a className="text-white/70 hover:text-pink-500" target="_blank"><div className="mt-4 flex items-center justify-center "><FaGithub className="text-xl inline-block"/><h1 className="ml-2 font-semibold inline-block">Contribute</h1></div></a></Link></div>
     </div>)
 }
 
