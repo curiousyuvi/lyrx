e018aaad076a4f2a0899f58c056e6df118b34283
Yuvraj Singh <ysgaur9919@gmail.com>
2022-01-30 03:09:37 +0530

    created lyrx-app

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import Footer from "../components/Footer";
+import Header from "../components/Header"
+
+export default function Home() {
+  return (
+    <>
+    <Header />
+      <div className="bg-grey-600 w-full min-h-screen">
+
+      </div>
+      <Footer/>
+    </>
+  )
+}
