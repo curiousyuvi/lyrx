63ba351c50faa2c6537f163db39a3f400a77d1f6
Yuvraj Singh <ysgaur9919@gmail.com>
2022-02-03 02:05:00 +0530

    feat: created api services to get country data and popular songs list

---
+++ services/endpoints.ts
@@ -0,0 +1,3 @@
+const MUSIXMATCH_ENDPOINT = "https://api.musixmatch.com/ws/1.1";
+
+export { MUSIXMATCH_ENDPOINT };
---
+++ services/get_country.ts
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+interface Country {
+  code: string;
+  name: string;
+}
+
+const getCountry = async function (): Promise<Country> {
+  try {
+    const response = await axios.get("https://ipapi.co/json/");
+    console.log(response.data);
+    return {
+      code: response.data.country_code,
+      name: response.data.country_name,
+    };
+  } catch (e) {
+    console.log("error in getting country", e);
+  }
+};
+
+export default getCountry;
+
+export type { Country };
