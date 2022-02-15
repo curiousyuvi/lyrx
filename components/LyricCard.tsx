5de0d0c364f6c7e1ff30d1d0b8d59d34dc00209b
Yuvraj Singh <ysgaur9919@gmail.com>
2022-02-15 22:14:54 +0530

    fix: centered icon and text verically in lyric card

--- components/LyricCard.tsx
+++ components/LyricCard.tsx
@@ -14,13 +14,13 @@ export default function LyricCard({ artist, title, id }) {
     return (
       <div className="bg-white h-40 rounded-md shadow-lg hover:shadow-xl flex flex-col justify-center relative overflow-hidden mx-2 my-4">
         <div className="p-2 pb-8">
-          <div className="flex items-start justify-start my-1">
+          <div className="flex items-center justify-start my-1">
             <FaMicrophoneAlt className="my-[7px] mr-2 text-indigo-400" />
             <p className="max-h-10 w-11/12 text-sm font-medium text-gray-600 overflow-hidden">
               {artist}
             </p>
           </div>
-          <div className="flex items-start justify-start my-1">
+          <div className="flex items-center justify-start my-1">
             <BsMusicNote className="my-[7px] mr-2 text-indigo-400" />
             <p className="max-h-10 w-11/12 text-sm font-medium text-gray-600 overflow-hidden">
               {title}
